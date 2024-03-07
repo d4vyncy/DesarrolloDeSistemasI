8f7e60a45dc3d69223ecc4e51b46e865f860212a
d4vyncy <davidserrudo@gmail.com>
2024-03-07 16:28:31 -0400

    actualizacion

---
+++ Ejemplo 01 - Aplicacion de consola/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero oHero = new Hero("spiderman", "telaraña");
+            Console.WriteLine(oHero.nombre);
+            Console.ReadLine();
+        }
+
+        class Hero
+        {
+            public string nombre;
+            public string poder;
+            public Hero(string pNombre, string pPoder)
+            {
+                this.nombre = pNombre;
+                this.poder = pPoder;
+            }
+        }
+    }
+}
